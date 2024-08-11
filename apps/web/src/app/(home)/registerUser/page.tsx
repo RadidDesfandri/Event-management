05fc4fb37fda25305560ba0ac469dc4c8620844c
Dietyy <radietdesfandri@gmail.com>
2024-08-11 22:08:57 +0700

    Radid (#19) (#20)
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Develop (#13)
    
    * Fix merge (#8)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Radid (#11)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * build: update (#12)
    
    
    
    
    ---------
    
    
    
    
    * Develop (#18)
    
    * build: update
    
    * build: merapikan file login dan register
    
    * Perbaikan responsif dan modal navbaar (#15)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Develop (#13)
    
    * Fix merge (#8)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Radid (#11)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * build: update (#12)
    
    
    
    
    ---------
    
    
    
    
    ---------
    
    
    
    
    * build: delete over file
    
    ---------
    
    
    
    
    * build: update page register
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>

--- apps/web/src/app/(home)/registerUser/page.tsx
+++ apps/web/src/app/(home)/registerUser/page.tsx
@@ -1,34 +1,8 @@
-"use client"
-import FormikRegisterUser from "@/components/register/formikRegisterUser";
-import LogoLg from "@/components/logoLg";
-import Image from "next/image";
-import Link from "next/link";
-
+import DesainRegisterEO from "@/components/register/desainRegister";
 export default function page() {
     return (
-        <div className='w-screen h-screen'>
-            <div className="flex justify-center pt-14">
-                <LogoLg />
-            </div>
-            <div className="flex w-full justify-between py-28">
-                <div className="flex flex-col h-96 w-1/2 justify-center items-center">
-                    <Image
-                        src="/images/welcomeImage.png"
-                        alt="logo"
-                        width={600}
-                        height={600}
-                    />
-                    <h1 className="font-bold pt-9">Tidak lagi ketinggalan event favoritmu</h1>
-                    <p>Gabung dan rasakan kemudahan bertransaksi dan mengelola event di tricket</p>
-                </div>
-                <div className="flex flex-col h-96 w-1/2 justify-center items-center ">
-                    <p className="font-bold">buat akun Tricket kamu</p>
-                    <p className="pb-4">sudah punya akun? <Link href={'/loginUser'} className="text-blue-800 font-bold">Masuk</Link></p>
-                    <div className="flex flex-col w-96 h-[475px] p-40 rounded-2xl shadow-xl justify-center items-center bg-gray-200">
-                        <FormikRegisterUser/>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <DesainRegisterEO link='/loginuser'/>
         </div>
     )
 }
