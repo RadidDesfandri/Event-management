acaac13d92eeb2804aa9929fb5f0e7cec03751ef
erlanggapeps <erlangga.prdn@gmail.com>
2024-08-11 22:15:33 +0700

    build: login user

--- apps/web/src/app/(home)/loginUser/page.tsx
+++ apps/web/src/app/(home)/loginUser/page.tsx
@@ -6,10 +6,10 @@ import FormikLoginUser from "../../../components/login/formikLoginUser";
 export default function page() {
     return (
         <div className='w-screen h-screen'>
-            <div className="flex justify-center pt-14">
+            <div className="flex flex-wrap justify-center pt-14">
                 <LogoLg />
             </div>
-            <div className="flex w-full justify-between py-28">
+            <div className="flex flex-wrap w-full justify-between py-28">
                 <div className="flex flex-col h-96 w-1/2 justify-center items-center">
                     <Image
                         src="/images/welcomeImage.png"
