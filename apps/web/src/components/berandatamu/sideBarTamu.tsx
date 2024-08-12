1a778b46e55e7d22212f3cc76de5d38d9a3145ad
Dietyy <radietdesfandri@gmail.com>
2024-08-12 19:32:37 +0700

    Combine radid to develop. Messege : Fix navbar login and register desain (#23)
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Develop (#13)
    
    * Fix merge (#8)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Radid (#11)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * build: update (#12)
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    * Develop (#18)
    
    * build: update
    
    * build: merapikan file login dan register
    
    * Perbaikan responsif dan modal navbaar (#15)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Develop (#13)
    
    * Fix merge (#8)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Radid (#11)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * build: update (#12)
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    * build: delete over file
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    * build: update page register
    
    * build: modal detail event
    
    * build: membuat navbar modal
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>

--- apps/web/src/components/berandatamu/sideBarTamu.tsx
+++ apps/web/src/components/berandatamu/sideBarTamu.tsx
@@ -8,8 +8,8 @@ export default function SideBarTamu() {
         <div className='hidden sticky top-20 md:flex flex-col justify-between w-1/12 h-full'>
             <div className='flex flex-col gap-8'>
                 <Link href={'/'}><MdHome className='w-9 h-9 hover:text-white transition-all duration-200 hover:bg-gray-800 rounded-md p-1 text-gray-300' /></Link>
-                <Link href={'/'}><IoIosCreate className='w-9 h-9 hover:text-white transition-all duration-200 hover:bg-gray-800 rounded-md p-1 text-gray-300' /></Link>
-                <Link href={'/'}><IoIosNotifications className='w-9 h-9 hover:text-white transition-all duration-200 hover:bg-gray-800 rounded-md p-1 text-gray-300' /></Link>
+                <Link href={'/'}><IoIosCreate className='w-9 h-9 hover:text-white cursor-not-allowed transition-all duration-200 hover:bg-gray-800 rounded-md p-1 text-gray-300' /></Link>
+                <Link href={'/'}><IoIosNotifications className='w-9 h-9 hover:text-white cursor-not-allowed transition-all duration-200 hover:bg-gray-800 rounded-md p-1 text-gray-300' /></Link>
             </div>
         </div>
     )
