242e7dab29d46b121665b7528a4b54a160da63c1
Dietyy <radietdesfandri@gmail.com>
2024-08-24 12:59:40 +0700

    Radid (#45)
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Develop (#13)
    
    * Fix merge (#8)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Radid (#11)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * build: update (#12)
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    * Develop (#18)
    
    * build: update
    
    * build: merapikan file login dan register
    
    * Perbaikan responsif dan modal navbaar (#15)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Develop (#13)
    
    * Fix merge (#8)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Radid (#11)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * Fix merge (#8) (#10)
    
    * build: desain berandatamu
    
    * build: update desain home and struktur beranda
    
    * build: desain create ticket
    
    * build: create prisma shema, create desain page detail event dan slug
    
    * build: update (#12)
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    * build: delete over file
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>
    
    * build: update page register
    
    * build: modal detail event
    
    * build: membuat navbar modal
    
    * build: fix desain navbar and all modal
    
    * build: buat controller dan router user, event
    
    * build: add fields firstname
    
    * build: update desain and formik create event
    
    * build: update schema and add controller events
    
    * build: update formik, and fetching all event
    
    * build: active interaktif create event and can preview ticket, and add image
    
    * build: detailevent and dinamic routes done
    
    * feat: debounce search
    
    * feat: update
    
    * feat: handle notfound
    
    * feat: controller create event and controller autheo
    
    * feat: updt
    
    * feat: loading comp
    
    ---------
    
    Co-authored-by: erlanggapeeps <erlangga.webd@gmail.com>
    Co-authored-by: erlanggapeps <erlangga.prdn@gmail.com>

--- apps/web/src/components/loading.tsx
+++ apps/web/src/components/loading.tsx
@@ -2,12 +2,27 @@ import React from 'react'
 
 export default function LoadingComp() {
     return (
-        <div className='w-full bg-black/90 '>
-            <div className='mx-auto flex justify-center h-screen max-w-7xl'>
-                <div className="flex justify-center items-center flex-row gap-2">
-                    <div className="w-3 h-3 rounded-full bg-gray-50 animate-bounce"></div>
-                    <div className="w-3 h-3 rounded-full bg-gray-50 animate-bounce [animation-delay:-.3s]"></div>
-                    <div className="w-3 h-3 rounded-full bg-gray-50 animate-bounce [animation-delay:-.5s]"></div>
+        <div className='w-full bg-gray-950 '>
+            <div className='mx-auto flex flex-col text-center justify-center h-screen max-w-7xl'>
+                <div className="flex justify-center items-center flex-row gap-1">
+                    <div className="rounded-full text-gray-300 text-3xl font-bold animate-bounce">t</div>
+                    <div className="rounded-full text-gray-300 text-3xl font-bold animate-bounce [animation-delay:-.3s]">r</div>
+                    <div className="rounded-full text-gray-300 text-3xl font-bold animate-bounce [animation-delay:-.5s]">i</div>
+                    <div className="rounded-full text-gray-300 text-3xl font-bold animate-bounce [animation-delay:-.6s]">c</div>
+                    <div className="rounded-full text-blue-500 text-3xl font-bold animate-bounce [animation-delay:-.5s]">k</div>
+                    <div className="rounded-full text-blue-500 text-3xl font-bold animate-bounce [animation-delay:-.6s]">e</div>
+                    <div className="rounded-full text-blue-500 text-3xl font-bold animate-bounce [animation-delay:-.3s]">t</div>
+                </div>
+                <div className="flex justify-center items-center flex-row gap-1">
+                    <div className="rounded-full text-blue-500 text-xs font-bold animate-bounce">t</div>
+                    <div className="rounded-full text-blue-500 text-xs font-bold animate-bounce [animation-delay:-.3s]">u</div>
+                    <div className="rounded-full text-blue-500 text-xs font-bold animate-bounce [animation-delay:-.5s]">n</div>
+                    <div className="rounded-full text-blue-500 text-xs font-bold animate-bounce [animation-delay:-.6s]">g</div>
+                    <div className="rounded-full text-gray-300 text-xs font-bold animate-bounce [animation-delay:-.5s]">g</div>
+                    <div className="rounded-full text-gray-300 text-xs font-bold animate-bounce [animation-delay:-.6s]">u</div>
+                    <div className="rounded-full text-gray-300 text-xs font-bold animate-bounce [animation-delay:-.3s]"></div>
+                    <div className="rounded-full text-gray-300 text-xs font-bold animate-bounce [animation-delay:-.3s]">y</div>
+                    <div className="rounded-full text-gray-300 text-xs font-bold animate-bounce [animation-delay:-.3s]">a</div>
                 </div>
             </div>
         </div>
